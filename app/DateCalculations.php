b616d90ab7632c715b689c17d38111886b02fbb3
dermiesh <65332461+dermiesh@users.noreply.github.com>
2020-05-14 03:39:28 -0500

    Update DateCalculations.php

--- app/DateCalculations.php
+++ app/DateCalculations.php
@@ -6,39 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class DateCalculations extends Model
 {
-	
-	public static function getuserData($id=0){
-
-    if($id==0){
-      $value=DB::table('date_calculations')->orderBy('id', 'desc')->get(); 
-    }else{
-      $value=DB::table('date_calculations')->where('id', $id)->first();
+    
+    public static function getuserData($id = 0)
+    {
+        
+        if ($id == 0) {
+            $value = DB::table('date_calculations')->orderBy('id', 'desc')->get();
+        } else {
+            $value = DB::table('date_calculations')->where('id', $id)->first();
+        }
+        return $value;
     }
-    return $value;
-  }
-
-  public static function insertData($data){
-    $value=DB::table('date_calculations')
-								->where('date1', $data['date1'])
-								->where('date2', $data['date2'])
-								->get();
-    if($value->count() == 0){
-      DB::table('date_calculations')->insert($data);
-      return 1;
-     }else{
-       return 0;
-     }
- 
-  }
-
-  public static function updateData($id,$data){
-    DB::table('date_calculations')
-      ->where('id', $id)
-      ->update($data);
-  }
-
-  public static function deleteData($id){
-    DB::table('date_calculations')->where('id', '=', $id)->delete();
-  }
+    
+    public static function insertData($data)
+    {
+        $value = DB::table('date_calculations')->where('date1', $data['date1'])->where('date2', $data['date2'])->get();
+        if ($value->count() == 0) {
+            DB::table('date_calculations')->insert($data);
+            return 1;
+        } else {
+            return 0;
+        }
+        
+    }
+    
+    public static function updateData($id, $data)
+    {
+        DB::table('date_calculations')->where('id', $id)->update($data);
+    }
+    
+    public static function deleteData($id)
+    {
+        DB::table('date_calculations')->where('id', '=', $id)->delete();
+    }
 }
-
