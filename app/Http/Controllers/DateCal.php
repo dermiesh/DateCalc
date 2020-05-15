55efc5f1ac9974ea22f12d76341ef04750518b90
dermiesh <65332461+dermiesh@users.noreply.github.com>
2020-05-15 08:11:10 -0500

    Update DateCal.php

--- app/Http/Controllers/DateCal.php
+++ app/Http/Controllers/DateCal.php
@@ -39,63 +39,70 @@ class DateCal extends Controller
         
         if ($request->input('submit') != null)
           {
+			$current_dt = Carbon::now()->toDateTimeString();
             $date1 = $request->input('date1');
             $date2 = $request->input('date2');
             
-            $to   = Carbon::parse($date1);
-            $from = Carbon::parse($date2);
-            
-            $day_amt = $to->diffInDays($from);
-            
-            // Update record
-            if ($request->input('editid') != null)
-              {
-                $editid = $request->input('editid');
-                //  var_dump($_POST);
-                
-                if ($date1 != '' && $date2 != '' && $day_amt != '' && $editid != '')
-                  {
-                    $data = array(
-                        'date1' => $date1,
-                        "date2" => $date2,
-                        "day_amt" => $day_amt
-                    );
-                    
-                    // Update
-                    DateCalculations::updateData($editid, $data);
-                    
-                    Session::flash('message', 'Update successfully.');
-                    
-                  }
-                
-              }
-            else // Insert record
-              {
-                
-                
-                if ($date1 != '' && $date2 != '' && $day_amt != '')
-                  {
-                    $data = array(
-                        'date1' => $date1,
-                        "date2" => $date2,
-                        "day_amt" => $day_amt
-                    );
-                    
-                    // Insert
-                    $value = DateCalculations::insertData($data);
-                    
-                    if ($value)
-                      {
-                        Session::flash('message', 'Insert successfully.');
-                      }
-                    else
-                      {
-                        Session::flash('message', 'Data already exists.');
-                      }
-                    
-                  }
-              }
-            
+			try {
+				$to   = Carbon::parse($date1);
+				$from = Carbon::parse($date2);
+				
+				$day_amt = $to->diffInDays($from);
+				
+				// Update record
+				if ($request->input('editid') != null)
+				  {
+					$editid = $request->input('editid');
+					//  var_dump($_POST);
+					
+					if ($date1 != '' && $date2 != '' && $day_amt != '' && $editid != '')
+					  {
+						$data = array(
+							'date1' => $date1,
+							"date2" => $date2,
+							"day_amt" => $day_amt,
+							"updated_at" => $current_dt
+						);
+						
+						// Update
+						DateCalculations::updateData($editid, $data);
+						
+						Session::flash('message', 'Update successfully.');
+						
+					  }
+					return redirect()->action('DateCal@index',['id'=>$editid]);
+				  }
+				else // Insert record
+				  {
+					
+					if ($date1 != '' && $date2 != '' && $day_amt != '')
+					  {
+						$data = array(
+							'date1' => $date1,
+							"date2" => $date2,
+							"day_amt" => $day_amt,
+							"created_at" => $current_dt,
+							"updated_at" => $current_dt
+							
+						);
+						
+						// Insert
+						$value = DateCalculations::insertData($data);
+						
+						if ($value)
+						  {
+							Session::flash('message', 'Insert successfully.');
+						  }
+						else
+						  {
+							Session::flash('message', 'Data already exists.');
+						  }
+						
+					  }
+				 }
+            } catch (\Exception $e) {
+				Session::flash('message', 'Invalid Date Structure.');
+			}
           }
 		  
         return redirect()->action('DateCal@index',['id'=>0]);
@@ -106,38 +113,44 @@ class DateCal extends Controller
         $_POST['statusCode'] = 201;
         if ($request->input('action') == 'Add')
           {
-            
-            
-            $date1   = $request->input('date1');
-            $date2   = $request->input('date2');
-            $day_amt = $request->input('day_amt');
-            
-            
-            // Insert record
-            
-            if ($date1 != '' && $date2 != '' && $day_amt != '')
-              {
-                $data = array(
-                    'date1' => $date1,
-                    "date2" => $date2,
-                    "day_amt" => $day_amt
-                );
-                
-                // Insert
-                $value = DateCalculations::insertData($data);
-                if ($value)
-                  {
-                    $_POST['statusCode'] = 200;
-                    Session::flash('message', 'Insert successfully.');
-                  }
-                else
-                  {
-                    Session::flash('message', 'Data already exists.');
-                  }
-                
-              }
+            try {
+				$current_dt = Carbon::now()->toDateTimeString();
+				$date1   = Carbon::parse($request->input('date1'))->format("Y-m-d");
+				$date2   = Carbon::parse($request->input('date2'))->format("Y-m-d");
+				$day_amt = $request->input('day_amt');
+				
+				
+				// Insert record
+				
+				if ($date1 != '' && $date2 != '' && $day_amt != '')
+				  {
+					$data = array(
+						'date1' => $date1,
+						"date2" => $date2,
+						"day_amt" => $day_amt,
+						"created_at" => $current_dt,
+						"updated_at" => $current_dt
+					);
+					
+					// Insert
+					$value = DateCalculations::insertData($data);
+					if ($value)
+					  {
+						$_POST['statusCode'] = 200;
+						Session::flash('message', 'Insert successfully.');
+					  }
+					else
+					  {
+						Session::flash('message', 'Data already exists.');
+					  }
+					
+				  }
+			}catch (\Exception $e) {
+				$_POST['statusCode'] = 201;
+				Session::flash('message', 'Invalid Date Structure.');
+			}
           }
-        
+		  
         echo json_encode($_POST);
         
       }
@@ -156,3 +169,4 @@ class DateCal extends Controller
         return redirect()->action('DateCal@index',['id'=>0]);
       }
   }
+  
